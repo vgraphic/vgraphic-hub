9d265aec0f0864c316991582a671373e19380dbb
vgraphic <bstricksxd@gmail.com>
2025-06-12 23:41:40 +0200

    fix sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://vgraphic.vercel.app', // ✅ Replace with your actual domain
+  generateRobotsTxt: true,           // ✅ Will also generate robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+};
